1f2506fc1a38b2aaab012133a617c30bc299ab80
Mateus Araujo <mateus-arao@hotmail.com>
2023-10-31 22:37:12 -0300

    create-calc

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import './App.css'
+import {useState} from 'react';
+import './App.css';
 
 function App() {
 
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [result, setResult] = useState(0);
+
+  console.log(weight, height);
+
+    const calculateImc = () => {
+    const imc = weight / (height * height);
+    const formattedImc = imc.toFixed(2);
+    setResult(+formattedImc);   
+  };
+
+  
   return (
     <>
       <h1>Calculadora de IMC</h1>
@@ -10,16 +24,17 @@ function App() {
           <label>
             Peso: 
             <div className='cardInput'>
-            <input type='text' value='Insira aqui o seu peso'></input>
+            <input type='number' value={weight} onChange={(e) => setWeight(+e.target.value)} placeholder='Peso (kg)'></input>
             </div>
           </label>
           <label>
             Altura: 
             <div className='cardInput'>
-            <input type='text' value='Insira aqui a sua altura'></input>
+            <input type='number' value={height} onChange={(e) => setHeight(+e.target.value)}  placeholder='Altura (m)'></input>
             </div>
           </label>
-          <button type="submit">Calcular</button>
+          <button type="submit" onClick={calculateImc}>Calcular</button>
+          <h1>{result}</h1>
         </div>       
       </div>
       
@@ -28,3 +43,4 @@ function App() {
 }
 
 export default App
+
