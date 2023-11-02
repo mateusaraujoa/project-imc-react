6a2b634c5b690b9838e324247a714e31a999c3da
Mateus Araujo <mateus-arao@hotmail.com>
2023-11-02 00:01:55 -0300

    feat: display imc

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,28 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <h1>Calculadora de IMC</h1>
       <div className="card">
         <div>
           <label>
-            Peso: 
-            <div className='cardInput'>
-            <input type='text' value='Insira aqui o seu peso'></input>
+            Peso:
+            <div className="cardInput">
+              <input type="text" value="Insira aqui o seu peso"></input>
             </div>
           </label>
           <label>
-            Altura: 
-            <div className='cardInput'>
-            <input type='text' value='Insira aqui a sua altura'></input>
+            Altura:
+            <div className="cardInput">
+              <input type="text" value="Insira aqui a sua altura"></input>
             </div>
           </label>
           <button type="submit">Calcular</button>
-        </div>       
+        </div>
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
