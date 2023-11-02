74118e9cbe3e9bf55b7fea0d2857761a9272302a
Mateus Araujo <mateus-arao@hotmail.com>
2023-11-01 23:56:27 -0300

    feat: create display imc

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,69 @@
-import {useState} from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
   const [weight, setWeight] = useState(0);
   const [height, setHeight] = useState(0);
   const [result, setResult] = useState(0);
+  const [message, setMessage] = useState("");
 
   console.log(weight, height);
 
-    const calculateImc = () => {
+  const calculateImc = () => {
     const imc = weight / (height * height);
-    const formattedImc = imc.toFixed(2);
-    setResult(+formattedImc);   
+    const formattedImc = Number(imc.toFixed(2));
+    setResult(formattedImc);
+    messageImc(formattedImc);
   };
 
-  
+  const messageImc = (formattedImc: number) => {
+    if (formattedImc < 18.5) {
+      setMessage("Abaixo do peso");
+    } else if (formattedImc < 25) {
+      setMessage("Peso normal");
+    } else if (formattedImc < 30) {
+      setMessage("Acima do peso");
+    } else {
+      setMessage("Obesidade");
+    }
+  };
+
   return (
     <>
       <h1>Calculadora de IMC</h1>
       <div className="card">
         <div>
           <label>
-            Peso: 
-            <div className='cardInput'>
-            <input type='number' value={weight} onChange={(e) => setWeight(+e.target.value)} placeholder='Peso (kg)'></input>
+            Peso:
+            <div className="cardInput">
+              <input
+                type="number"
+                value={weight}
+                onChange={(e) => setWeight(+e.target.value)}
+                placeholder="Peso (kg)"
+              ></input>
             </div>
           </label>
           <label>
-            Altura: 
-            <div className='cardInput'>
-            <input type='number' value={height} onChange={(e) => setHeight(+e.target.value)}  placeholder='Altura (m)'></input>
+            Altura:
+            <div className="cardInput">
+              <input
+                type="number"
+                value={height}
+                onChange={(e) => setHeight(+e.target.value)}
+                placeholder="Altura (m)"
+              ></input>
             </div>
           </label>
-          <button type="submit" onClick={calculateImc}>Calcular</button>
+          <button type="submit" onClick={calculateImc}>
+            Calcular
+          </button>
           <h1>{result}</h1>
-        </div>       
+          <h1>{message}</h1>
+        </div>
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
